eb90608493021584a969c0a9a311dc8efcfae6b1
NikitaKis <900940@mail.ru>
2019-03-17 21:40:04 +0400

    server/get recipe details

--- server/handler.js
+++ server/handler.js
@@ -2,19 +2,23 @@
 const rp = require("request-promise");
 
 const headers = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Credentials": true
+  Authorization: `Token ${process.env.TOKEN}`,
+  Accept: "application/json"
 };
 
 function successResponse(params) {
-  return { statusCode: 200, headers, body: JSON.stringify(params) };
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    },
+    body: JSON.stringify(params)
+  };
 }
 
 module.exports.recipes_search = (event, context, callback) => {
-  const headers = {
-    Authorization: `Token ${process.env.TOKEN}`,
-    Accept: "application/json"
-  };
+  // TODO make input validation
   var queryParamsStr = "https://graph.whisk.com/v1/search";
   if (event.queryStringParameters) {
     var queryParams = Object.keys(event.queryStringParameters).map(function(key) {
@@ -23,6 +27,31 @@ module.exports.recipes_search = (event, context, callback) => {
     });
     queryParamsStr += "?" + queryParams.join("&");
   }
+  const paramsGet = {
+    uri: queryParamsStr,
+    method: "GET",
+    headers,
+    json: true,
+    timeout: 10000,
+    agent: false
+  };
+  rp(paramsGet)
+    .then(response => {
+      console.log("response is ", JSON.stringify(response, null, 2));
+      callback(null, successResponse({ ...response }));
+    })
+    .catch(err => {
+      console.log("err is ", err);
+      callback(err);
+    });
+};
+module.exports.recipe = (event, context, callback) => {
+  // TODO make input validation
+  var queryParamsStr = "https://graph.whisk.com/v1/";
+  const id = event.queryStringParameters.id;
+  if (!id) return callback("No id");
+  queryParamsStr += id + "?fields=normalizedIngredients,instructions,nutrition";
+
   const paramsGet = {
     uri: queryParamsStr,
     method: "GET",
